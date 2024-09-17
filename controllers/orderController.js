73189ae1e1149ca2fffd4f7e15ebcd04194e48ae
hoannhan2003 <nguyenhoannhan.nnn@gmail.com>
2024-09-17 11:04:13 +0700

    add

---
+++ controllers/orderController.js
@@ -0,0 +1,51 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const frontend_url = "https://localhost:5173"
+
+//placing user order
+const placeOrder = async (req, res) => {try {
+    const newOrder = new orderModel({
+      userId: req.body.userId,
+      items: req.body.items,
+      amount: req.body.amount,
+      address: req.body.address,
+    });
+    await newOrder.save();
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
+    const line_items = req.body.items.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+  
+    line_items.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: 2 * 100 * 80,
+      },
+      quantity:1
+    });
+    const session = await stripe.checkout.session.create({
+      line_items:line_items,
+      mode:'payment',
+      succes_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    })
+    res.json({succes:true,session_url:session.url})
+  } catch (error) {
+    console.log(error);
+    res.json({succes:false,message:"Error"})
+  }};
+export { placeOrder };
